3bd792c94afad1c411111e55d4a8d0e35aabf731
Masroor Rasheed <masroor@me.com>
2020-01-20 14:50:46 +0000

    Sway

--- sway.sh
+++ sway.sh
@@ -6,15 +6,15 @@ echo "${green}**************************************************"
 echo "*** Installing Packages for Sway"
 echo "**************************************************${reset}"
 
-sudo pacman -S sway wlroots wayland swaylock swayidle waybar ttf-liberation
-#sudo pacman -Syu xorg-server-xwayland   #Backward Compatiblity with X11
+sudo pacman -S sway waybar ttf-liberation 
+sudo pacman -Syu xorg-server-xwayland   #Backward Compatiblity with X11
 #sudo pacman -Syu lxapperance  # Customization of GTK+
-sudo pacman -S termite mako awesome  awesome-terminal-fonts 
-pikaur -S wofi-hg
+sudo pacman -S termite mako awesome  awesome-terminal-fonts rofi firefox
 
 echo "${green}**************************************************"
 echo "*** Moving Configurations"
 echo " 1 - Sway"
 echo " 2 - Termite (Terminal Emulator)"
 echo "**************************************************${reset}"
+mkdir ${HOME}/.config/sway
 cp ${SHELL_PATH}/config/sway/config ${HOME}/.config/sway/
