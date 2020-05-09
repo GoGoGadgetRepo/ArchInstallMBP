df163ed728e48449e76ddd771bf2f3da366aa76c
Masroor Rasheed <masroor.rasheed@gmail.com>
2020-05-09 17:44:27 +0100

    More Corrections

--- post_install.sh
+++ post_install.sh
@@ -52,15 +52,15 @@ pikaur --noconfirm -S thermald
 sudo systemctl enable thermald
 sudo systemctl start thermald
 
-#pikaur --noconfirm -S laptop-mode-tools
-#sudo systemctl enable laptop-mode.service
-#sudo systemctl start laptop-mode.service
+pikaur --noconfirm -S laptop-mode-tools
+sudo systemctl enable laptop-mode.service
+sudo systemctl start laptop-mode.service
 
-#info "Enabling fans for the Macbook Pro"
-#pikaur --noconfirm -S mbpfan-git kbdlight
-#sudo cp mbpfan.conf /etc/
-#sudo systemctl enable mbpfan
-#sudo systemctl start mbpfan
+info "Enabling fans for the Macbook Pro"
+pikaur --noconfirm -S mbpfan kbdlight
+sudo cp ${SHELL_PATH}/config/etc/mbpfan.conf /etc/
+sudo systemctl enable mbpfan
+sudo systemctl start mbpfan
 
 
 info "Installing Missing Firmware and Update Linux Kernel"
