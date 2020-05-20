847eb407f2283ef469748c693d7f172a6f59942a
Masroor Rasheed <masroor.rasheed@gmail.com>
2020-05-20 14:09:20 +0100

    Setting up Gnome install

--- gnome.sh
+++ gnome.sh
@@ -1,7 +1,29 @@
+#!/usr/bin/bash
+# Defining the shell path and global variables 
+SHELL_PATH=$(readlink -f $0 | xargs dirname)
+source ${SHELL_PATH}/scripts/global.sh
+
 
 sudo pacman --noconfirm -Syu gnome gnome-extra gnome-tweaks gdm
+
+sudo pacman --noconfirm -Syu awesome awesome-terminal-fonts ttf-liberation  
+sudo pacman --noconfirm -Syu firefox 		# Web Browser
+sudo pacman --noconfirm -Syu alacritty		# Terminal 
+
+info "Zathura PDF and Plug-ins"
+sudo pacman --noconfirm -Syu zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps
+
+info "Multimedia frameworks"
+sudo pacman --noconfirm -Syu gst-libav gst-plugins-base gst-plugins-good libde265 gstreamer-vaapi
+
+info "Setting packages that enable Wayland GUI and Video Acceleration"
+sudo pacman --noconfirm -S qt5-wayland glfw-wayland glew-wayland clutter
+sudo pacman --noconfirm -S intel-media-driver libva-intel-driver libvdpau-va-gl libva-utils
 
 gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
 
 sudo systemctl enable gdm
 sudo systemctl start gdm
+
+# Alacritty
+ln -s ${SHELL_PATH}/config/alacritty ${HOME}/.config
