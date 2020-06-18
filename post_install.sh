29309150dbf0cc396836a6cb4dd5bd964ed82bb3
Masroor Rasheed <masroor.rasheed@gmail.com>
2020-06-18 12:51:47 +0100

    Adding zsh-theme-powerlevel10k pacakge to support zsh prompt

--- post_install.sh
+++ post_install.sh
@@ -40,7 +40,7 @@ pikaur --noconfirm -S wd719x-firmware aic94xx-firmware bcwc-pcie-git
 sudo mkinitcpio -p linux
 
 info "Installing Utilities"
-sudo pacman --noconfirm -S zsh man pacman-contrib zsh-syntax-highlighting htop nnn vlc youtube-dl lm_sensors unzip ttf-liberation imv bat
+sudo pacman --noconfirm -S zsh man pacman-contrib zsh-syntax-highlighting htop nnn vlc youtube-dl lm_sensors unzip ttf-liberation imv bat zsh-theme-powerlevel10k
 
 info "Applying custom settings."
 sh ${SHELL_PATH}/settings.sh
