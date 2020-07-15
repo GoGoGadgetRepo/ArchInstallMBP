e37f7f9e9fe06ce3d05090841073c5e38a904fe0
Masroor Rasheed <masroor.rasheed@gmail.com>
2020-07-15 13:43:34 +0100

    Removed the top comment line

--- scripts/global.sh
+++ scripts/global.sh
@@ -6,8 +6,8 @@ reset=`tput sgr0`
 
 # Screen Printing of the information passed
 info() {
-	echo "${orange}"
-	printf %${COLUMNS}s | tr " " "="
+	#echo "${orange}"
+	#printf %${COLUMNS}s | tr " " "="
 	echo "${green}$1${orange}"
 	printf %${COLUMNS}s | tr " " "="
 	echo "${reset}"
