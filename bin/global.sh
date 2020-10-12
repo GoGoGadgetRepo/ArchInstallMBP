c7ce5efb3e2a0b2b501f185ebebb891da52ac82c
Masroor Rasheed <masroor.rasheed@gmail.com>
2020-10-12 17:10:14 +0100

    Moved the global file to bin

--- bin/global.sh
+++ bin/global.sh
@@ -6,9 +6,23 @@ reset=`tput sgr0`
 
 # Screen Printing of the information passed
 info() {
-	echo "${orange}"
-	printf %${COLUMNS}s | tr " " "="
+	#echo "${orange}"
+	#printf %${COLUMNS}s | tr " " "="
 	echo "${green}$1${orange}"
 	printf %${COLUMNS}s | tr " " "="
 	echo "${reset}"
+}
+# Show an informative Error
+error() {
+	echo "${red}$1${red}"
+	printf %${COLUMNS}s | tr " " "="
+	echo "${reset}"
+	exit 12
+}
+install() {
+	[ -f $1 ] && info "Installing Packages from $1" || error "$1 is not a file."
+
+	sudo pacman -Syyuu --noconfirm
+	sudo pacman -S --noconfirm $(awk 'BEGIN{ORS=" "} {print $0}' $1)
+	
 }
